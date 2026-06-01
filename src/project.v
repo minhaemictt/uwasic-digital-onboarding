ff43afa027ce4a2b450c2a76cce9bf892c1be0e0
Tony <tony.dinh@coasia.com>
2026-06-01 14:44:11 +0700

    Fix module name

--- src/project.v
+++ src/project.v
@@ -5,7 +5,7 @@
 
 `default_nettype none
 
-module tt_um_uwasic_onboarding_minh (
+module tt_um_uwasic_onboarding_minhaemictt  (
     input  wire [7:0] ui_in,
     output wire [7:0] uo_out,
     input  wire [7:0] uio_in,
